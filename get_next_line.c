f0cd83b8ad28981622d50dd0a3f803d48c10b34e
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r9s8.wethinkcode.co.za>
2019-07-10 15:50:17 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 11:00:25 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/07/10 15:14:14 by lmolaodi         ###   ########.fr       */
+/*   Updated: 2019/07/10 15:49:43 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 char	ft_check_inside_file(int fd, int red_fd, char **file, char **buff)
 {
-	int i;
+	int		i;
 	char	*tp;
 
 	i = 0;
@@ -28,7 +28,7 @@ char	ft_check_inside_file(int fd, int red_fd, char **file, char **buff)
 		free(file[fd]);
 		file[fd] = tp;
 	}
-	if(file[fd][i] == '\0')
+	if (file[fd][i] == '\0')
 	{
 		if (red_fd == BUFF_SIZE)
 			return (get_next_line(fd, buff));
@@ -49,8 +49,8 @@ int		get_next_line(const int fd, char **line)
 
 	if (fd == -1 || line == NULL)
 		return (-1);
-		if (file[fd] == NULL)
-			file[fd] = ft_strnew(0);
+	if (file[fd] == NULL)
+		file[fd] = ft_strnew(0);
 	while ((red_fd = read(fd, text, BUFF_SIZE)) > 0)
 	{
 		text[red_fd] = '\0';
