d0b9f00012d226ebc1b52bf91a1d314ad55f5a22
Lehlogonolo Morgan MOLAODI <lmolaodi@c5r9s8.wethinkcode.co.za>
2019-07-15 15:33:46 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -5,16 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmolaodi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/08 11:00:25 by lmolaodi          #+#    #+#             */
-/*   Updated: 2019/07/12 12:54:00 by lmolaodi         ###   ########.fr       */
+/*   Created: 2019/07/15 13:31:47 by lmolaodi          #+#    #+#             */
+/*   Updated: 2019/07/15 15:33:14 by lmolaodi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	ft_check_inside_file(int fd, char **file, char **buff)
+char	ft_check_line(int fd, char **file, char **buff)
 {
-	char	*tp;
+	char	*tmp;
 	int		i;
 
 	i = 0;
@@ -23,9 +23,9 @@ char	ft_check_inside_file(int fd, char **file, char **buff)
 	if (file[fd][i] == '\n')
 	{
 		*buff = ft_strsub(file[fd], 0, i);
-		tp = ft_strdup(file[fd] + i + 1);
-		ft_strdel(&file[fd]);
-		file[fd] = tp;
+		tmp = ft_strdup(file[fd] + i + 1);
+		free(file[fd]);
+		file[fd] = tmp;
 	}
 	else
 	{
@@ -35,14 +35,14 @@ char	ft_check_inside_file(int fd, char **file, char **buff)
 	return (1);
 }
 
-int		get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
-	static char		*file[1024];
-	char			text[BUFF_SIZE + 1];
-	char			*temp;
-	int				red_fd;
+	static char	*file[1024];
+	char		text[BUFF_SIZE + 1];
+	char		*temp;
+	int			red_fd;
 
-	if (fd == -1 || line == NULL)
+	if (fd == -1 || line == NULL || read(fd, text, 0))
 		return (-1);
 	if (file[fd] == NULL)
 		file[fd] = ft_strnew(0);
@@ -50,14 +50,15 @@ int		get_next_line(const int fd, char **line)
 	{
 		text[red_fd] = '\0';
 		temp = ft_strjoin(file[fd], text);
-		ft_strdel(&file[fd]);
+		free(file[fd]);
 		file[fd] = temp;
 		if (ft_strchr(file[fd], '\n'))
 			break ;
 	}
 	if (red_fd == 0 && file[fd][0] == '\0')
+	{
 		*line = NULL;
-	else
-		return (-1);
-	return (ft_check_inside_file(fd, file, line));
+		return (0);
+	}
+	return (ft_check_line(fd, file, line));
 }
